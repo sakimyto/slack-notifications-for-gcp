5916ca9805edd9ca1e5978dac3d333b5a9bf8ff1
sakimyto <21049958+sakimyto@users.noreply.github.com>
2020-06-23 18:32:58 +0900

    Upgrade to use budget consumption rate

--- README.rst
+++ README.rst
@@ -1,24 +1,36 @@
-==================
-Alert_GCP-badget_to_slack
-==================
+========================================================================
+Alert_GCP-badget_to_slack / salckにGCP消化予算を通知する
+========================================================================
+
+.. image::https://user-images.githubusercontent.com/21049958/85386606-c7c05980-b57e-11ea-8ae3-d07a06305c72.png
 
 
+*********************************************
+Getting Started / スタートガイド
+*********************************************
 
-Install
-============
+Prerequisites / 必要条件
+====================================
+- GCP
+- Python 3.7.2
+
+
+Installing / インストール
+====================================
 
 
 1.Setting budget and alerts with Pub/Sub topics in GCP
 -------------------------------------------------------------
 
-https://console.cloud.google.com/billing
+`GCP billing <https://console.cloud.google.com/billing>`_
 
 
 2. Creat a slack bot in slack api
 -----------------------------------------
 
 
-1. Access to https://api.slack.com and creat a bot
+1. Access to `Slack API <https://api.slack.com>`_ and creat a bot
+
 
 2. In "App Home" Add, "App Display Name"
 
@@ -42,3 +54,27 @@ https://console.cloud.google.com/billing
 -------------------------------------------------------------------------------------------
 
 complete!!
+
+
+*********************************************
+Authors / 著者
+*********************************************
+`sakimyto <https://twitter.com/sakimyto>`_
+
+*********************************************
+License / ライセンス
+*********************************************
+
+This is under `MIT license <https://en.wikipedia.org/wiki/MIT_License>`_ .
+
+*********************************************
+Acknowledgments / 謝辞
+*********************************************
+
+References / 参考・引用文献
+====================================
+
+- `Examples of automated cost control responses | Google Cloud <https://cloud.google.com/billing/docs/how-to/notify>`_
+- `GCP予算アラートをSlackに通知する | Qiita <https://qiita.com/takezoux2/items/0cff8a2cc4f900cf1d29>`_
+
+
--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import json
 import base64
-from slack import WebClient
-from slack.errors import SlackApiError
+import datetime
+import dateutil.parser
+import slack
+
+BOT_ACCESS_TOKEN = 'xxxx-111111111111-abcdefghidklmnopq'
+CHANNEL_ID = 'C0XXXXXX'
+
+slack_client = slack.WebClient(token=BOT_ACCESS_TOKEN)
 
 
 def notify_slack(data, context):
-    ACCESS_TOKEN = 'xxxxxxxxxxxxxxxx'
-    CHANNEL_ID = 'xxxxxx'
-    client = WebClient(token=ACCESS_TOKEN)
-
     try:
         notification_data = base64.b64decode(data['data']).decode('utf-8')
         notification_data = json.loads(notification_data)
-        budget_notification_text = '予算額：{0} ({1})\n利用料金：{2} ({1})\n利用開始日時：{3}'.format(
-            notification_data.get('budgetAmount'),
-            notification_data.get('currencyCode'),
-            notification_data.get('costAmount'),
-            notification_data.get(
-                'costIntervalStart'))
-        response = client.chat_postMessage(
-            channel=CHANNEL_ID,
-            text=budget_notification_text)
-        assert response['message']['text'] == budget_notification_text
 
-    except SlackApiError as e:
+        today = datetime.datetime.now().day
+        first_day = dateutil.parser.parse(notification_data.get('costIntervalStart')).day
+        budgetAmount = notification_data.get('budgetAmount')
+        currencyCode = notification_data.get('currencyCode')
+        costAmount = notification_data.get('costAmount')
+        now_pace = costAmount / int(today - first_day)
+        schedule_pace = budgetAmount / 30
+        if now_pace > schedule_pace:
+            # in English
+            budget_notification_text = '<!channel> \nBudget consumption rate is too fast.\n\n'
+            budget_notification_text += 'BudgetAmount：' + str(budgetAmount) + ' (' + str(currencyCode) + ')\n'
+            budget_notification_text += 'CostAmount：' + str(costAmount) + ' (' + str(currencyCode) + ')\n\n'
+            budget_notification_text += 'BudgetAmount per day：' + str(round(schedule_pace)) + ' (' + str(
+                currencyCode) + ')\n'
+            budget_notification_text += 'CostAmount per day：' + str(round(now_pace)) + ' (' + str(
+                currencyCode) + ')\n\n'
+            budget_notification_text += 'Please check your GCP'
+
+            # in Japanese
+            # budget_notification_text = '<!channel> \n予算に対して、1日の利用料金ペースが上回っています！\n\n'
+            # budget_notification_text += '設定している予算額：' + str(budgetAmount) + ' (' + str(currencyCode) + ')\n'
+            # budget_notification_text += '現在の消化予算：' + str(costAmount) + ' (' + str(currencyCode) + ')\n\n'
+            # budget_notification_text += '1日あたり予定予算：' + str(round(schedule_pace)) + ' (' + str(currencyCode) + ')\n'
+            # budget_notification_text += '1日あたり消化予算：' + str(round(now_pace)) + ' (' + str(currencyCode) + ')\n\n'
+            # budget_notification_text += '念のため確認しておきましょう！'
+
+            response = slack_client.chat_postMessage(
+                channel=CHANNEL_ID,
+                text=budget_notification_text)
+            assert response['message']['text'] == budget_notification_text
+
+    except slack.errors.SlackApiError as e:
         assert e.response['ok'] is False
         assert e.response['error']
         print(f"Got an error: {e.response['error']}")
